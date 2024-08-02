8fff7134c53a542a059549580196495bdf051705
sakurament <coo7ki8end-@ezweb.ne.jp>
2024-08-02 23:55:43 +0900

    フラッシュメッセージのエラー

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -75,7 +75,8 @@ class OwnersController extends Controller
 
     return redirect()
     ->route('admin.owners.index')
-    ->with('message', 'オーナー登録を実施しました。');
+    ->with(['message' => 'オーナー登録を実施しました。',
+    'status' => 'info']);
     }
 
     /**
@@ -119,7 +120,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しました。');
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -130,6 +132,12 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        dd('削除処理');
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message'=> 'オーナー情報を削除しました。', 
+        'status' => 'alert']);
+    
     }
 }
