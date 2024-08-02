120ee9e7d3d9896f83520e04e98bfd0dfbd60141
sakurament <coo7ki8end-@ezweb.ne.jp>
2024-08-03 02:03:51 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -140,4 +140,15 @@ class OwnersController extends Controller
         'status' => 'alert']);
     
     }
+
+    public function expiredOwnerIndex(){ 
+        $expiredOwners = Owner::onlyTrashed()->get(); 
+        return view('admin.expired-owners', 
+    compact('expiredOwners')); 
+    }
+
+    public function expiredOwnerDestroy($id){ 
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete(); 
+        return redirect()->route('admin.expired-owners.index');  
+    }    
 }
